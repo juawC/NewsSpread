adba595f5d300724a5d8c0a2dc09580ac99c5a79
João Cevada <joaocevada@gmail.com>
2017-12-24 15:57:24 +0000

    Added age to article list item view.

--- app/src/main/java/com/juawapps/newsspread/utils/biding/BindingAdapters.java
+++ app/src/main/java/com/juawapps/newsspread/utils/biding/BindingAdapters.java
@@ -1,8 +1,16 @@
 package com.juawapps.newsspread.utils.biding;
 
+import android.content.res.Resources;
 import android.databinding.BindingAdapter;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.view.View;
+import android.widget.TextView;
+
+import com.juawapps.newsspread.R;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Data Binding adapters specific to the app.
@@ -15,9 +23,36 @@ public class BindingAdapters {
     public static void setAdapter(RecyclerView view, RecyclerView.Adapter adapter) {
         view.setAdapter(adapter);
     }
+
     @BindingAdapter("recyclerItemDecoration")
     public static void addItemDecoration(RecyclerView view, RecyclerView.ItemDecoration decoration) {
         view.addItemDecoration(decoration);
+    }
+
+    @BindingAdapter("setAgeFromDate")
+    public static void setAgeFromDate(TextView view, Date date) {
+
+        if (date == null) {
+            view.setText(null);
+            return;
+        }
+
+        long currentTimeMs = Calendar.getInstance().getTimeInMillis();
+        long dateTimeMs = date.getTime();
+        long age = currentTimeMs - dateTimeMs;
+
+        int minutes = (int) (age / DateUtils.MINUTE_IN_MILLIS);
+        int hours = (int) (age / DateUtils.HOUR_IN_MILLIS);
+        int days = (int) (age / DateUtils.DAY_IN_MILLIS);
+
+        Resources resources = view.getResources();
+        if (days > 0) {
+            view.setText(resources.getQuantityString(R.plurals.days, days, days));
+        } else if (hours > 0) {
+            view.setText(resources.getQuantityString(R.plurals.hours, hours, hours));
+        } else {
+            view.setText(resources.getQuantityString(R.plurals.minutes, minutes, minutes));
+        }
     }
 
     @BindingAdapter("setVisible")
